74da8425a1dfb925768ed821ac8d25b4ac26f77e
caiocv18 <caioviniciuscv18@gmail.com>
2019-11-14 12:22:05 -0300

    Inserindo um For

--- src/br/com/caiozin/main/Executora.java
+++ src/br/com/caiozin/main/Executora.java
@@ -4,6 +4,9 @@ public class Executora {
 
 	public static void main(String[] args) {
 		System.out.println("MENSAGEM DE TESTE");
-	}
+		for (int i = 0; i < 10; i++) {
+			System.out.println(i);
+		}
 
+	}
 }
